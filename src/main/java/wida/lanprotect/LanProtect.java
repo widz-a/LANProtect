8a9951beb94086465041d9838e709c3dd9e1115e
Wida <itswithermc@gmail.com>
2025-05-31 23:04:35 +0700

    Initalized Project

---
+++ src/main/java/wida/lanprotect/LanProtect.java
@@ -0,0 +1,10 @@
+package wida.lanprotect;
+
+import net.fabricmc.api.ModInitializer;
+
+public class LanProtect implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/wida/lanprotect/client/LanProtectClient.java
@@ -0,0 +1,10 @@
+package wida.lanprotect.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LanProtectClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
